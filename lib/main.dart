f3ca070dee59743c696d00dfac3f850c077ad968
Foudroyant <stephanebazebibouta@gmail.com>
2023-12-23 14:33:08 +0100

    correction des beugs de notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,75 @@
 import 'dart:ui';
 
+//import OneSignal
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_background_service/flutter_background_service.dart';
-import 'package:flutter_background_service_android/flutter_background_service_android.dart';
-import 'package:workmanager/workmanager.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+
 import 'package:ringtone_player/ringtone_player.dart';
 import 'firebase_options.dart';
 import 'dart:async';
 import 'dart:convert';
 
-
-
-// this will be used as notification channel id
-const notificationChannelId = 'basic_channel';
-const notificationId = 0;
-
-/*
-@pragma('vm:entry-point') // Mandatory if the App is obfuscated or using Flutter 3.1+
-void callbackDispatcher() {
-  Workmanager().executeTask((task, inputData) {
-    print("Native called background task: $task"); //simpleTask will be emitted here.
-    notif("Une nouvelle navigation sur tel site.");
-
-    return Future.value(true);
-  });
-}
-*/
-
-notif(String body){
+notif(String titlte, String body){
   AwesomeNotifications().createNotification(content: NotificationContent(
       id: 0,
       channelKey :"basic_channel",
-      title: "Nouvelle navigation",
+      title: titlte,
       body: body
   ));
+  FlutterRingtonePlayer().playNotification();
+}
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+  notif("${message.notification?.title}",'${message.notification?.body}');
+
+  print("Handling a background message: ${message.messageId}");
+  print("Handling a background message: ${message.notification?.title}");
+  print("Handling a background message: ${message.notification?.body}");
+}
+
+Future<void> initPush() async {
+  await FirebaseMessaging.instance.subscribeToTopic("nouvelle_navigation").then((value) => print("Nouvelle navigation"));
+
+  FirebaseMessaging.instance.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  //final token = await messaging.getToken();
+
+  //print('User granted permission: ${settings.authorizationStatus} : ${token}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message}');
+
+    final notification = message.notification;
+    if (notification != null) {
+      print('Message also contained a notification: ${notification.title}');
+      print('Message also contained a notification: ${notification.body}');
+      notif("${notification.title}",'${notification.body}');
+    }
+  });
 }
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-  /*Workmanager().initialize(
-      callbackDispatcher, // The top level function, aka callbackDispatcher
-      isInDebugMode: true // If enabled it will post a notification whenever the task is running. Handy for debugging tasks
-  );*/
+  //f65ca766-4b02-4d67-b70a-9839e5e6faca
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -58,79 +79,9 @@ void main() async{
       importance: NotificationImportance.Max, playSound: true, onlyAlertOnce: true, criticalAlerts: true
   ),
   ], debug: true);
-  await initializeService();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
   runApp(const MyApp());
 }
-
-Future<void> initializeService() async{
-  final service = FlutterBackgroundService();
-
-  await service.configure(
-      androidConfiguration: AndroidConfiguration(
-        // this will be executed when app is in foreground or background in separated isolate
-        onStart: onStart,
-
-        // auto start service
-        autoStart: true,
-        isForegroundMode: true,
-
-        notificationChannelId: notificationChannelId, // this must match with notification channel you created above.
-        initialNotificationTitle: 'AWESOME SERVICE',
-        initialNotificationContent: 'Initializing',
-        foregroundServiceNotificationId: notificationId,
-      ),
-      iosConfiguration: IosConfiguration(
-        // auto start service
-        autoStart: true,
-
-        // this will be executed when app is in foreground in separated isolate
-        onForeground: onStart,
-
-        // you have to enable background fetch capability on xcode project
-        onBackground: onIosBackground,
-      ),
-  );
-}
-
-@pragma('vm:entry-point')
-Future<void> onStart(ServiceInstance service) async {
-  // Only available for flutter 3.0.0 and later
-  DartPluginRegistrant.ensureInitialized();
-
-  if (service is AndroidServiceInstance) {
-    service.on('setAsForeground').listen((event) {
-      service.setAsForegroundService();
-    });
-
-    service.on('setAsBackground').listen((event) {
-      service.setAsBackgroundService();
-    });
-  }
-
-  service.on('stopService').listen((event) {
-    service.stopSelf();
-  });
-
-  // bring to foreground
-  Timer.periodic(const Duration(seconds: 10), (timer) async {
-    if (service is AndroidServiceInstance) {
-      if (await service.isForegroundService()) {
-        print("BACKGROUND ANDROID");
-      }
-    }
-  });
-  print("HORS TIMER");
-}
-
-@pragma('vm:entry-point')
-Future<bool> onIosBackground(ServiceInstance service) async {
-  WidgetsFlutterBinding.ensureInitialized();
-  DartPluginRegistrant.ensureInitialized();
-  print("BACKGROUND IOS");
-
-  return true;
-}
-
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -144,7 +95,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Djowyett Notif'),
+      home: BottomMenuExample(),
     );
   }
 }
@@ -165,6 +116,8 @@ class _MyHomePageState extends State<MyHomePage> {
   late SharedPreferences _prefs;
   final List<String> processedDocumentIds = [];
   final player = AudioPlayer();
+  bool _enableConsentButton = false;
+  String _debugLabelString = "";
 
   Future<void> _initSharedPreferences() async {
     _prefs = await SharedPreferences.getInstance();
@@ -213,9 +166,19 @@ class _MyHomePageState extends State<MyHomePage> {
     ));
   }
 
+  initOneSignal() async{
+    OneSignal.shared.setLogLevel(OSLogLevel.verbose, OSLogLevel.none);
+    OneSignal.shared.setAppId("f65ca766-4b02-4d67-b70a-9839e5e6faca");
+    OneSignal.shared.promptUserForPushNotificationPermission().then((value){
+      print(value);
+    });
+  }
+
   @override
   void initState() {
     super.initState();
+    //initOneSignal();
+    initPush();
     _initSharedPreferences();
     AwesomeNotifications().isNotificationAllowed()
     .then((isAllowed){
@@ -223,21 +186,6 @@ class _MyHomePageState extends State<MyHomePage> {
         AwesomeNotifications().requestPermissionToSendNotifications();
       }
     });
-    final service = FlutterBackgroundService();
-    service.startService();
-    FlutterBackgroundService().invoke("setAsForeground");
-
-    //Workmanager().registerOneOffTask("task-identifier", "simpleTask");
-    /*Workmanager().registerPeriodicTask("Tache-Periodique", "Tache-Periodique",
-      initialDelay: Duration(seconds: 20),
-      frequency: Duration(minutes: 5),
-        constraints: Constraints(
-          // connected or metered mark the task as requiring internet
-          networkType: NetworkType.connected,
-          // require external power
-          requiresCharging: true,
-        )
-    );*/
   }
 
   @override
@@ -252,10 +200,6 @@ class _MyHomePageState extends State<MyHomePage> {
     FirebaseFirestore.instance.collection('visites').orderBy('time', descending: true).snapshots();
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
       body: StreamBuilder(
           stream: _visitesStream,
           builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot){
@@ -284,8 +228,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 // Vérifiez si le document a déjà été traité
                 if (!processedDocumentIds.contains(documentId)) {
                   // Ajoutez l'ID à la liste des documents traités
-                  _playNotificationSound(documentId);
-                  lancerNotification("${newDocument["name"]} a été visité à ${newDocument["time"]}");
+                  //_playNotificationSound(documentId);
+                  //lancerNotification("${newDocument["name"]} a été visité à ${newDocument["time"]}");
                   // Effectuez votre traitement ici pour le nouvel objet
                   print("Nouveau site visité : ${newDocument["name"]}");
                 }
@@ -324,6 +268,186 @@ class _MyHomePageState extends State<MyHomePage> {
                 });
           }
       )// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class GestionSites extends StatefulWidget {
+  @override
+  _GestionSitesState createState() => _GestionSitesState();
+}
+
+class _GestionSitesState extends State<GestionSites> {
+  TextEditingController _textFieldController = TextEditingController();
+  final CollectionReference _sitesCollection = FirebaseFirestore.instance.collection('sites');
+  late List<DocumentSnapshot> _sites = [];
+
+  Future<void> _chargerDonnees() async {
+    var result = await _sitesCollection.get();
+    setState(() {
+      _sites = result.docs;
+    });
+  }
+
+  void _supprimerSite(String siteId) async {
+    try {
+      await _sitesCollection.doc(siteId).delete();
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Site supprimé avec succès.'),
+        ),
+      );
+
+      _chargerDonnees(); // Recharge les données après la suppression
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erreur lors de la suppression du site : $e'),
+        ),
+      );
+    }
+  }
+
+  void _ajouterSite(String nomDuSite) async {
+    try {
+      await _sitesCollection.add({
+        'name': nomDuSite,
+      });
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Site ajouté avec succès.'),
+        ),
+      );
+
+      _chargerDonnees(); // Recharge les données après l'ajout
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erreur lors de l\'ajout du site : $e'),
+        ),
+      );
+    }
+  }
+
+  void _showAddDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Ajouter un site'),
+          content: TextField(
+            controller: _textFieldController,
+            decoration: InputDecoration(hintText: 'Nom du site'),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Annuler'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Ajouter'),
+              onPressed: () {
+                _ajouterSite(_textFieldController.text);
+                _textFieldController.clear();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _chargerDonnees();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _sites != null
+          ? ListView.builder(
+        itemCount: _sites.length,
+        itemBuilder: (context, index) {
+          var site = _sites[index];
+          return Card(child: ListTile(
+            title: Text(site['name']),
+            trailing: IconButton(
+              icon: Icon(Icons.delete),
+              color: Theme.of(context).colorScheme.error,
+              onPressed: () => _supprimerSite(site.id),
+            ),
+          ),);
+        },
+      )
+          : Center(
+        child: CircularProgressIndicator(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showAddDialog(context),
+        tooltip: 'Ajouter',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+}
+
+
+class BottomMenuExample extends StatefulWidget {
+  @override
+  _BottomMenuExampleState createState() => _BottomMenuExampleState();
+}
+
+class _BottomMenuExampleState extends State<BottomMenuExample> {
+  int _currentPageIndex = 0;
+
+  final List<Widget> _pages = [
+    MyHomePage(title: "Djowyett Notif"),
+    GestionSites()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Djowyett Notif"),
+      ),
+      body: PageView(
+        children: _pages,
+        onPageChanged: (index) {
+          //print(index);
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        onTap: (index) {
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Accueil',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.web),
+            label: 'Sites',
+          ),
+        ],
+      ),
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -11,7 +11,7 @@ import awesome_notifications_core
 import cloud_firestore
 import firebase_core
 import firebase_database
-import flutter_local_notifications
+import firebase_messaging
 import path_provider_foundation
 import shared_preferences_foundation
 
@@ -22,7 +22,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
